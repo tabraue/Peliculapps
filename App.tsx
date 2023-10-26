225a39fac118f8da06fe00e9dbc107a2e887a58c
Diana Tabraue <dianatabraue@gmail.com>
2023-10-26 11:13:15 +0100

    fade started w customhook

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigation/StackNavigator';
+import FadeScreen from './src/screens/FadeScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      {/* <StackNavigator/> */}
+      <FadeScreen/>
     </NavigationContainer>
   )
 }
---
+++ src/screens/FadeScreen.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Animated, Button, View} from 'react-native';
+import { useFade } from '../hooks/useFade';
+
+const FadeScreen = () => {
+
+    const {opacity, fadeIn, fadeOut} = useFade()
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'grey',
+      }}>
+      <Animated.View
+        style={{
+          backgroundColor: 'powderblue',
+          width: 150,
+          height: 150,
+          borderColor: 'pink',
+          borderWidth: 10,
+          opacity,
+          marginBottom: 10
+        }}
+      />
+      <Button title='Fade In' onPress={fadeIn}/>
+      <Button title='Fade Out' onPress={fadeOut}/>
+    </View>
+  );
+};
+
+export default FadeScreen;
